cbeb0fc527e11b2c0e59b57e602cf5bfd9b02675
John Sahhar <john@entropy.xyz>
2022-11-04 21:37:34 +0000

    Add rust toolchain to CI step
    Signed-off-by: John Sahhar <john@entropy.xyz>

--- example/ci-test.sh
+++ example/ci-test.sh
@@ -5,6 +5,14 @@ readonly NODE_OS="linux"
 readonly NODE_ARCH="x64"
 readonly FN="node-$NODE_VERSION-$NODE_OS-$NODE_ARCH"
 
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+rustup update nightly
+rustup update stable
+rustup default stable
+rustup target add wasm32-unknown-unknown --toolchain nightly
+cargo install wasm-pack
+
 # Compile/link typescript library
 make
 make link
