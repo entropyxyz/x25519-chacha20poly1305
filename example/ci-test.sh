5db1393937fd6cc0badaa6c7ac14952b196e071d
John Sahhar <john@entropy.xyz>
2022-11-04 21:30:55 +0000

    fix ci test
    Signed-off-by: John Sahhar <john@entropy.xyz>

--- example/ci-test.sh
+++ example/ci-test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-readonly EXEC_DIR=$(dirname "$(realpath $0)") && cd $__EXEC_DIR
+readonly EXEC_DIR=$(dirname "$(realpath $0)") 
 readonly NODE_VERSION="v18.12.1"
 readonly NODE_OS="linux"
 readonly NODE_ARCH="x64"
 readonly FN="node-$NODE_VERSION-$NODE_OS-$NODE_ARCH"
 
+# Install JS dependencies
+cd $EXEC_DIR
 wget https://nodejs.org/dist/$NODE_VERSION/$FN.tar.xz
 tar xf $FN.tar.xz
 export PATH=$PATH:$EXEC_DIR/$FN/bin
 npm install -g ts-node
 
+# Run tests
 ts-node test.ts
 
